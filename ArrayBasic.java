f0b107b9ae60b3561e5d4277c60277328c0fce25
JulaiWyn <julaiwyn.dev@gmail.com>
2024-08-21 21:52:57 +0630

    Update ArrayBasic.java

--- ArrayBasic.java
+++ ArrayBasic.java
@@ -1,2 +1,18 @@
 class ArrayBasic {
+  public static void main(String[] args) {
+    int[] numbers = new int[5];
+    numbers [0] = 45;
+    numbers [1] = 62;
+    numbers [2] = 18;
+    numbers [3] = 32;
+    numbers [4] = 96;
 
+    System.out.println(numbers[0]);
+    System.out.println(numbers[1]);
+    System.out.println(numbers[2]);
+    System.out.println(numbers[3]);
+    System.out.println(numbers[4]);
+
+    System.out.println("Array Size : " + numbers.length);
+  }
+} 
